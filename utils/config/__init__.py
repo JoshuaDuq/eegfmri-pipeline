d86f7c023e5d676cf637d48f5479aa1658d46f40
Joshua Duquette <joshua.duquette.1@ulaval.ca>
2025-11-25 17:55:26 -0500

    Delete utils directory

--- utils/config/__init__.py
+++
@@ -1,5 +0,0 @@
-from .loader import load_settings, load_config, ConfigDict, get_config_value, get_constants, ensure_config
-
-__all__ = ["load_settings", "load_config", "ConfigDict", "get_config_value", "get_constants", "ensure_config"]
-
-
